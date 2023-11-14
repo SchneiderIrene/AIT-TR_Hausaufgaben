5dc188842ad712205785133562ed79702f7247c2
Schneider Irene <schneider_irene@outlook.com>
2023-11-14 21:51:44 +0100

    Hausaufgaben_6_01/0_2 kurzer

--- src/hausaufgaben_06/HA_06_02.java
+++ src/hausaufgaben_06/HA_06_02.java
@@ -4,10 +4,10 @@ public class HA_06_02 {
     public static void main(String[] args) {
         int a,b,c;
         a = 3;
-        b = 1;
+        b = 3;
         c = 3;
 
-       if (a==b & b==c & a==c){
+       if (a==b & a==c){
            System.out.println("All numbers are equal");
        } else if (a==b | b==c | a==c) {
            System.out.println("Some numbers are equal");
