fe574d629b0c5ad01c227e1cec719efcef9ab9b2
Schneider Irene <schneider_irene@outlook.com>
2023-11-15 19:05:09 +0100

    Hausaufgaben_7_01/0_1 with  can not divide by 0!!!

--- src/hausaufgaben_06/HA_06_01.java
+++ src/hausaufgaben_06/HA_06_01.java
@@ -4,14 +4,22 @@ public class HA_06_01 {
     public static void main(String[] args) {
         int a,b;
         a = 3;
-        b = 3;
+        b = 5;
+        int sum = a+b;
 
-       if (a==b){
-           System.out.println((a+b)*2);
-       } else {
-           System.out.println(a+b);
-       }
+        if (a==b){
+            sum = 2*sum;
+        }
+        System.out.println(sum);
 
+//        a = 3;
+//        b = 3;
+//
+//       if (a==b){
+//           System.out.println((a+b)*2);
+//       } else {
+//           System.out.println(a+b);
+//       }
 
     }
 }
