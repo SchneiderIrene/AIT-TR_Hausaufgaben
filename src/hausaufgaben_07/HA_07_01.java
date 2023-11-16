7cdb96d4e449db3eff5ce42c79d78e2ae24ec3c1
Schneider Irene <schneider_irene@outlook.com>
2023-11-16 10:57:26 +0100

    Hausaufgaben_7_01/0_1 double

--- src/hausaufgaben_07/HA_07_01.java
+++ src/hausaufgaben_07/HA_07_01.java
@@ -4,37 +4,37 @@ import java.util.Scanner;
 
 public class HA_07_01 {
     public static void main(String[] args) {
-        Scanner scannerI = new Scanner(System.in);
+        Scanner scannerD = new Scanner(System.in);
         Scanner scannerS = new Scanner(System.in);
 
         System.out.println("Enter first number");
-        int a = scannerI.nextInt();
+        double a = scannerD.nextDouble();
 
         System.out.println("Enter arithmetic sign");
         String sign = scannerS.nextLine();
 
         System.out.println("Enter second number");
-        int b = scannerI.nextInt();
+        double b = scannerD.nextDouble();
 
 
         switch (sign){
             case "-":
-                System.out.println(a-b);
+                System.out.println(a + " " + sign + " " + b + " = " + (a-b));
                 break;
 
             case "+":
-                System.out.println(a+b);
+                System.out.println(a + " " + sign + " " + b + " = " +(a+b));
                 break;
 
             case "*":
-                System.out.println(a*b);
+                System.out.println(a + " " + sign + " " + b + " = " +(a*b));
                 break;
 
             case "/":
                 if(b==0){
                     System.out.println("You can not divide by 0!!!");
                 } else {
-                    System.out.println(a/b);
+                    System.out.println(a + " " + sign + " " + b + " = " +(a/b));
                 }
 
                 break;
