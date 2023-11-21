09904d62f7e428a62e871a35098b56feda201b71
Schneider Irene <schneider_irene@outlook.com>
2023-11-21 11:09:13 +0100

    Hausaufgaben_10_01/0_1 _2 with return

--- src/hausaufgaben_10/HA_10_02.java
+++ src/hausaufgaben_10/HA_10_02.java
@@ -5,33 +5,35 @@ import java.util.Scanner;
 public class HA_10_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int secretNumber = 5;
-        int a = 0;
-        int n = 5;
+        int secretNumber = 57;
+        int attempts = 10;
         int answer = 0;
 
-        while (a<n){
-            System.out.println("Guess a number between 0 and 9");
+        while (attempts>0){
+            System.out.println("Guess a number between 0 and 100");
             answer = scanner.nextInt();
-            a++;
+            attempts--;
 
             if (answer == secretNumber){
                 System.out.println("You are right! You have guessed the secret number!");
-                break;
+                return;
             }
             else {
                 System.out.print("Sorry, the number  ");
+
                 if(answer<secretNumber){
-                    System.out.println("is too small");
+                    System.out.println("is too small.");
                 }
                 else {
                     System.out.println("is too big");
                 }
+                System.out.println("You have " + attempts + " try" );
             }
+
         }
-        if (answer!=secretNumber){
+//        if (answer!=secretNumber){
             System.out.println("Sorry, you have no more try");
-        }
+//        }
 
     }
 }
