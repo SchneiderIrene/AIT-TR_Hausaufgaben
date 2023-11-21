c9ff4ec2c8390382403fb5bd970e97295cd3f3df
Schneider Irene <schneider_irene@outlook.com>
2023-11-21 20:41:34 +0100

    Hausaufgaben_11_01/02 во внешнем loop убрала печать counter, только переход на новую строку

--- src/hausaufgaben_11/HA_11_02.java
+++ src/hausaufgaben_11/HA_11_02.java
@@ -2,26 +2,25 @@ package hausaufgaben_11;
 
 public class HA_11_02 {
     public static void main(String[] args) {
-        triangle(6);
+        triangle(10);
     }
 
-    static void triangle (int n){
+    static void triangle(int n) {
         int counter = 1;
-        while (counter<=n){
-            System.out.println(counter);
+        while (counter <= n) {
             counter++;
+            System.out.println();
 
             int counter2 = 1;
-            while (counter2<counter) {
+            while (counter2 < counter) {
                 System.out.print(counter2);
                 counter2++;
             }
         }
-        }
-
-
 
     }
+
+}
 
 
 
