e950684d1353eafa434177518956df870b5bbc58
Schneider Irene <schneider_irene@outlook.com>
2023-11-28 16:18:35 +0100

    hausaufgaben 14

--- src/hausaufgaben_14/HA_14_01.java
+++ src/hausaufgaben_14/HA_14_01.java
@@ -2,9 +2,6 @@ package hausaufgaben_14;
 
 public class HA_14_01 {
     public static void main(String[] args) {
-//        Есть массив целых чисел. Написать метод,
-//        находящий и возвращающий количество четных чисел в этом массиве.
-//        Пример: {1, 2, 8, 7} -> количество четных чисел = 2.
     int arr [] = {1,2,8,7};
         System.out.println(sumElArr(arr));
 
