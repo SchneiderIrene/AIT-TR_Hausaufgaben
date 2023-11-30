873c50cbd01974b3c841d25a0211bd7afc1ed08a
Schneider Irene <schneider_irene@outlook.com>
2023-11-30 09:39:55 +0100

    hausaufgaben 15 для общих слуаев

--- src/hausaufgaben_15/HA_15_01.java
+++ src/hausaufgaben_15/HA_15_01.java
@@ -5,18 +5,20 @@ import java.util.Arrays;
 public class HA_15_01 {
     public static void main(String[] args) {
         int []arr = {10,15,10,1,25,10};
-        System.out.println("Summa elements '10' array " + Arrays.toString(arr) + "  = 30? " + sum10(arr));
-
+        int n = 15;
+        int m = 30;
+        System.out.println("Summa elements " +  n + " array " +
+                Arrays.toString(arr) + "  = " + m + "? " + sum10(arr, n, m));
 
     }
-    public  static boolean sum10 (int []arr) {
+    public  static boolean sum10 (int []arr, int n, int m) {
         int sum = 0;
         for (int el : arr) {
 
-            if (el == 10) {
+            if (el == n) {
                 sum += el;
             }
         }
-        return sum == 30;
+        return sum == m;
     }
 }
