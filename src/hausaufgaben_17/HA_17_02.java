062d679b5045ef5a17caa485af9c0b449e03347b
Schneider Irene <schneider_irene@outlook.com>
2023-12-04 10:23:10 +0100

    hausaufgaben 17_01 + 1Method

--- src/hausaufgaben_17/HA_17_02.java
+++ src/hausaufgaben_17/HA_17_02.java
@@ -5,6 +5,7 @@ public class HA_17_02 {
         String str = "hello";
         int index = 2;
         System.out.println(removeCharAtIndex(str, index));
+        System.out.println(removeCharAtIndex2(str, index));
 
     }
 
@@ -23,10 +24,19 @@ public class HA_17_02 {
         String strRes = "";
         for (int i = 0; i < chars.length; i++) {
             if (i != index) {
-                strRes += chars[i];
+                strRes += chars[i]; // str.substring(0, index) + str.substring(index + 1);
             }
         }
         return strRes;
+    }
+
+    public static String removeCharAtIndex2(String str, int index) {
+
+        if (index >= str.length() || index<0){
+            return "Such index does not exist";
+        }
+        return str.substring(0, index) + str.substring(index + 1);
+
     }
 }
 
