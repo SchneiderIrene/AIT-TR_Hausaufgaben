4e46b413cf88b6a514148170abf05790f9e95db2
Schneider Irene <schneider_irene@outlook.com>
2023-12-12 11:40:48 +0100

    hausaufgaben 22

--- src/hausaufgaben_22/Calculator.java
+++ src/hausaufgaben_22/Calculator.java
@@ -2,20 +2,40 @@ package hausaufgaben_22;
 
 public class Calculator {
 
-    public double sum (double a, double b){
+    public  double sum (double a, double b){
         System.out.print(a + " + " + b + " = ");
         return a+b;
     }
-    public double sum (double a, double b, double c){
+    public  double sum (double a, double b, double c){
         System.out.print(a + " + " + b + " + " + c + " = ");
         return a+b+c;
     }
 
-    public double sub (double a, double b){
+    public  int sum (int a, int b){
+        System.out.print(a + " + " + b + " = ");
+        return a+b;
+    }
+    public  int sum (int a, int b, int c) {
+        System.out.print(a + " + " + b + " + " + c + " = ");
+        return a + b + c;
+    }
+
+
+
+
+    public  double sub (double a, double b){
         System.out.print(a + " - " + b + " = ");
         return a-b;
     }
-    public double sub (double a, double b, double c){
+    public  double sub (double a, double b, double c){
+        System.out.print(a + " - " + b + " - " + c + " = ");
+        return a-b-c;
+    }
+    public  int sub (int a, int b){
+        System.out.print(a + " - " + b + " = ");
+        return a-b;
+    }
+    public  int sub (int a, int b, int c){
         System.out.print(a + " - " + b + " - " + c + " = ");
         return a-b-c;
     }
